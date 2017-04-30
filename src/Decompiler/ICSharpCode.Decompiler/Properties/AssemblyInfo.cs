abb18fa7df19930055e00feea9007d9088e93ec7
William Kent <wjk@sunsol.me>
2017-04-30 18:43:59 -0400

    Add ICSharpCode.Decompiler project

---
+++ src/Decompiler/ICSharpCode.Decompiler/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("ICSharpCode.Decompiler")]
+[assembly: AssemblyCompany("Sunburst Solutions")]
+[assembly: AssemblyProduct("dotNETSpy")]
+[assembly: AssemblyCopyright("Copyright © 2017 William Kent. All rights reserved.")]
+[assembly: AssemblyVersion("1.0.0.0")]
