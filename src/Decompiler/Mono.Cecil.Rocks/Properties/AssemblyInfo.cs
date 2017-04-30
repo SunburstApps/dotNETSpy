f7641f45591975c7fba8fb7a1e5c3f91001a0c5f
William Kent <wjk@sunsol.me>
2017-04-30 18:25:20 -0400

    Add Mono.Cecil.Rocks project

---
+++ src/Decompiler/Mono.Cecil.Rocks/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("Mono.Cecil.Rocks")]
+[assembly: AssemblyCompany("Sunburst Solutions")]
+[assembly: AssemblyProduct("dotNETSpy")]
+[assembly: AssemblyCopyright("Copyright © 2017 William Kent. All rights reserved.")]
+[assembly: AssemblyVersion("1.0.0.0")]
