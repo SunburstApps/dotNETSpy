d127d998f6e1bdc03943d3a32d7ab26e3ad20633
William Kent <wjk@sunsol.me>
2017-04-30 17:38:49 -0400

    Add Mono.Cecil project

---
+++ src/Decompiler/Mono.Cecil/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Mono.Cecil")]
+[assembly: AssemblyCompany("Sunburst Solutions")]
+[assembly: AssemblyProduct("dotNETSpy")]
+[assembly: AssemblyCopyright("Copyright © 2017 William Kent. All rights reserved.")]
+
+[assembly: AssemblyVersion("1.0.0.*")]
