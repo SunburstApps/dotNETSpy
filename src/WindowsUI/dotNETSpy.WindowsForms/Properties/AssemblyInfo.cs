60f21ee01dafea221b9909a622874a44eb9d59a6
William Kent <wjk011@gmail.com>
2017-05-05 18:59:11 -0400

    Add Windows Forms-based front-end project

---
+++ src/WindowsUI/dotNETSpy.WindowsForms/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("dotNETSpy")]
+[assembly: AssemblyCompany("Sunburst Solutions")]
+[assembly: AssemblyProduct("dotNETSpy")]
+[assembly: AssemblyCopyright("Copyright © 2017 William Kent. All rights reserved.")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
