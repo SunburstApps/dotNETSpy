76b21d64f378cfd16a4d50e97e88da38744a5334
William Kent <wjk011@gmail.com>
2017-05-06 17:31:12 -0400

    Use the AppToolStripRenderer
    This change greatly improves the aesthetics of the MainForm's
    menu bar when one of its items is checked. (The default Windows
    Forms checkmark graphic is horrendously ugly.)

--- src/Windows/dotNETSpy/MainForm.cs
+++ src/Windows/dotNETSpy/MainForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using dotNETSpy.Controls;
 
 namespace dotNETSpy
 {
@@ -15,6 +16,7 @@ namespace dotNETSpy
         public MainForm()
         {
             InitializeComponent();
+            MainMenuStrip.Renderer = new AppToolStripRenderer();
         }
     }
 }
